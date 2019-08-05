c2ab3ec618175b3077f8c3e8d93c658b75d5ed9b
Jacob Asiedu <jasiedu@broadinstitute.org>
2019-08-05 12:03:15 -0400

    Testing 123

--- src/index.php
+++ src/index.php
@@ -15,10 +15,10 @@
     <body>
         <div class="container">
             <div class="hero-unit">
-                <h1>Simple PHP App</h1>
-                <h2>Congratulations</h2>
-                <p>Your PHP application is now running on a container in Amazon ECS.</p>
-                <p>The container is running PHP version <?php echo phpversion(); ?>.</p>
+                <h1>Simple PHP App2</h1>
+                <h2>Congratulations2</h2>
+                <p>Your PHP application 2 is now running on a container in Amazon ECS.</p>
+                <p>The container is running 2 PHP version <?php echo phpversion(); ?>.</p>
                 <?php
                         $myfile = fopen("/var/www/my-vol/date", "r") or die("");
                         echo fread($myfile,filesize("/var/www/my-vol/date"));
